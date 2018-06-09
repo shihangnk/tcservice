fc000beb978002abc33f3ee079a5a5e42b98b5ab
SeanShi <seans@iqmetrix.com>
2018-06-08 19:49:39 -0600

    refactor

--- src/com/tcservice/CenterService.java
+++ src/com/tcservice/CenterService.java
@@ -2,33 +2,39 @@ package com.tcservice;
 
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.validation.constraints.*;
 
 import com.tcservice.dao.CenterDao;
 import com.tcservice.model.Center;
 
 // case sensitive
-@Path("/centerservice")
+@Path("/centers")
 public class CenterService {
 	CenterDao centerDao = new CenterDao();
 
 	@GET
-	@Path("/centers")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Center> getUsers() {
 		return centerDao.getAllCenters();
 	}
 
 	@GET
-	@Path("/centers/{param}")
+	@Path("/{param}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Center getMsg(@PathParam("param") String msg) {
-
 		String output = "Jersey say : " + msg;
 		return new Center(2);
 	}
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+    @NotNull(message = "{contact.already.exist}")
+    public Response addContact(final Center center) {
+    	center.setId(center.getId()+1);
+    	return Response.status(201).entity(center).build();
+    }
+
 }
